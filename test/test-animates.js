7476d2935aa4fc84cc5f28b1cd67f5dc8f264bf8
alanclarke <hi@alz.so>
2016-03-31 11:23:12 +0100

    use fake timers & add tests for stop method

--- test/test-animates.js
+++ test/test-animates.js
@@ -1,29 +1,44 @@
-/* globals describe beforeEach it */
+/* globals describe beforeEach afterEach it */
 var animates = require('../lib/animates')
 var expect = require('chai').expect
+var sinon = require('sinon')
 
 describe('animate', function () {
-  var el
+  var el, clock
 
   beforeEach(function () {
+    clock = sinon.useFakeTimers()
     el = document.createElement('div')
     el.setAttribute('style', 'width: 20px; height: 20px; background: black;')
     document.body.appendChild(el)
   })
 
-  it('should camel case style declarations', function (done) {
+  afterEach(function () {
+    clock.restore()
+  })
+
+  it('should camel case style declarations', function () {
     animates(el, { opacity: 0 }, { speed: 100 })
-    setTimeout(middle, 10)
-    setTimeout(end, 200)
+    clock.tick(1)
+    expect(el.style.transition).to.eql('opacity 0.1s')
+    clock.tick(100)
+    expect(el.style.opacity).to.eql('0')
+    expect(el.style.transition).to.eql('')
+  })
 
-    function middle () {
-      expect(el.style.transition).to.eql('opacity 0.1s')
-    }
-
-    function end () {
-      expect(el.style.opacity).to.eql('0')
-      expect(el.style.transition).to.eql('none')
-      done()
-    }
+  it('should be cancelable', function () {
+    var style = el.getAttribute('style')
+    var stop = animates(el, { opacity: 0 }, { speed: 100 })
+    stop()
+    clock.tick(101)
+    expect(el.getAttribute('style')).to.eql(style)
+  })
+  it('should remove transitions if appropriate', function () {
+    el.style.opacity = 0
+    var style = el.getAttribute('style')
+    var stop = animates(el, { opacity: 0 }, { speed: 100 })
+    clock.tick(10)
+    stop()
+    expect(el.getAttribute('style')).to.eql(style)
   })
 })
